b5b1937f3ae9036c2754124ae8d66e48d95fdcef
arifmuliawan <42100291+arifmuliawan@users.noreply.github.com>
2024-08-09 15:29:31 +0700

    Initial Commit

---
+++ administator/template/admin.php
@@ -0,0 +1,238 @@
+<?php
+$query      = mysqli_query($con,"SELECT * from admin WHERE visible!='D' order by sortid ASC")or die (mysqli_error($con));
+$sum_query  = mysqli_num_rows($query);
+if(isset($action))
+{
+    if($action=='4')
+    {
+        $update     = mysqli_query($con,"UPDATE admin SET visible='D',update_by='$user',update_date='$now' WHERE id='$id'")or die (mysqli_error($con));
+        $query_del  = mysqli_query($con,"SELECT * from admin WHERE id='$id'")or die (mysqli_error($con));
+        $data_del   = mysqli_fetch_array($query_del);
+        $user_del   = $data_del['username'];
+        if($update==1)
+        {
+            //update Log//
+            $log_menu   = "Admin Management";
+            $log_action = "Delete";
+            $log_text   = "Delete ".$user_del." Successfully!";
+            $update_log = mysqli_query($con,"INSERT into `log` (`menu`,`action`,`log`,`create_by`,`create_date`) VALUES ('$log_menu','$log_action','$log_text','$user','$now')")or die (mysqli_error($con));
+            echo "
+                    <script type='text/javascript'>
+                        $(window).on('load', function() {
+                            $('#deletemodal').modal('show');
+                        });
+                        var delay = 2000;
+                        setTimeout(function(){ window.location ='index.php?p=admin'; }, delay);
+                    </script>
+                    ";
+        }
+    }
+}
+?>
+        <!-- /.modal -->
+        <div class="modal fade" id="deletemodal">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-body" style="text-align: center;vertical-align: middle;padding: 40px;">
+                        <img src="<?php echo $base_assets ?>dist/img/icon_success.png" style="width: 70px;">
+                        <br><br>
+                        <h5> Data Berhasil Dihapus </h5> 
+                    </div>
+                </div>
+                <!-- /.modal-content -->
+            </div>
+            <!-- /.modal-dialog -->
+        </div>
+        <!-- /.modal -->
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper" style="background-color: #ffffff;">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 style="margin: 24px">USER MANAGEMENT</h1>
+                        </div>
+                    </div>
+                </div><!-- /.container-fluid -->
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card" style="box-shadow: unset;">
+                                <div class="card-header" style="border-bottom: unset;">
+                                    <div class="row">
+                                        <div class="col-md-10">
+                                            
+                                        </div>
+                                        <div class="col-md-2" style="text-align: end;">
+                                            <?php
+                                            if(in_array(2,$arr_usracc))
+                                            {
+                                            ?>    
+                                                <a href="index.php?p=admin_form&a=2"><button type="button" class="btn" style="background-color: #88A8D4;color: #ffffff;"><i class="fa fa-plus"></i> New Data</button></a>
+                                            <?php
+                                            }
+                                            ?>    
+                                        </div>
+                                    </div>
+                                </div>        
+                                <!-- /.card-header -->
+                                <div class="card-body">
+                                    <table class="table" style="text-align: center;">
+                                    <thead style="background-color: #88A8D4;color: #ffffff;">
+                                        <tr>
+                                            <th>Username</th>
+                                            <th>Password</th>
+                                            <th>Last Login</th>
+                                            <th>Last Logout</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="row_position">
+                                        <?php
+                                        if($sum_query>0)
+                                        {
+                                            $no=1;
+                                            while($data_query=mysqli_fetch_array($query))
+                                            {
+                                                $id             = $data_query['id'];
+                                                $sortid         = $data_query['sortid'];
+                                                $username       = $data_query['username'];
+                                                $password       = "**********";
+                                                $access         = $data_query['access'];
+                                                $last_login_dt  = $data_query['last_login'];
+                                                if($last_login_dt=='2001-01-01 00:00:00')
+                                                {
+                                                    $last_login = '';
+                                                }
+                                                else
+                                                {
+                                                    $last_login = $last_login_dt;
+                                                }
+                                                $last_logout_dt = $data_query['last_logout'];
+                                                if($last_logout_dt=='2001-01-01 00:00:00')
+                                                {
+                                                    $last_logout= '';
+                                                }
+                                                else
+                                                {
+                                                    $last_logout= $last_logout_dt;
+                                                }
+                                                $visible        = $data_query['visible'];
+                                                if($visible=='Y')
+                                                {
+                                                    $icon_visible   = 'fa fa-unlock';
+                                                    $color_visible  = 'success';
+                                                    $text_visible   = 'Active';
+                                                    $check_visible  = '';
+                                                    $value_visible  = 'N';
+                                                }
+                                                else
+                                                {
+                                                    $icon_visible   = 'fa fa-lock';
+                                                    $color_visible  = 'danger';
+                                                    $text_visible   = 'InActive';
+                                                    $check_visible  = 'checked';
+                                                    $value_visible  = 'Y';
+                                                }
+                                                if ($no % 2 == 0)
+                                                {
+                                                    $clr_row = "background-color: #D9D9D9B2;";
+                                                }
+                                                else
+                                                {
+                                                    $clr_row = "background-color: #ffffff;";
+                                                }
+                                        ?>
+                                                <tr style="<?php echo $clr_row ?>" class="tableid" id="<?php echo $sortid ?>" data-id="<?php echo $id ?>" data-sec="<?php echo $sortid ?>">
+                                                    <td><?php echo $username ?></td>
+                                                    <td><?php echo $password ?></td>
+                                                    <td><?php echo $last_login ?></td>
+                                                    <td><?php echo $last_logout ?></td>
+                                                    <td style="min-width: 130px;display: flex;align-items: center;justify-content: center;">
+                                                        <?php
+                                                        if(in_array(3,$arr_usracc))
+                                                        {
+                                                        ?>    
+                                                            <a href="index.php?p=admin_form&a=3&id=<?php echo $id ?>">
+                                                                <button type="button" class="btn btn-warning"><i class="fa fa-edit"></i></button>
+                                                            </a>
+                                                        <?php
+                                                        }
+                                                        ?>    
+                                                        &nbsp&nbsp&nbsp
+                                                        <?php
+                                                        if(in_array(4,$arr_usracc))
+                                                        {
+                                                        ?>
+                                                            <a href="index.php?p=admin&a=4&id=<?php echo $id ?>" onclick="return confirm('Are you sure you want to delete this item?');">
+                                                                <button type="button" class="btn btn-danger"><i class="fa fa-trash"></i></button>
+                                                            </a>
+                                                        <?php
+                                                        }
+                                                        ?>  
+                                                        <!--<button type="button" class="btn btn-sm btn-<?php echo $color_visible ?>" style="pointer-events: none;"><i class="<?php echo $icon_visible ?>"></i> <?php echo $text_visible ?> </button>-->
+                                                        <?php
+                                                        if(in_array(3,$arr_usracc))
+                                                        {
+                                                            $stt_form   ='';
+                                                        }
+                                                        else
+                                                        {
+                                                            $stt_form   ='disabled';   
+                                                        }    
+                                                        ?>
+                                                        <form id="checkgo" method="post" style="margin-left: 12px;">
+                                                            <input type="hidden" name="id" value="<?php echo $id ?>" id="id<?php echo $id ?>">
+                                                            <input type="hidden" name="tbname" value="admin" id="tbname<?php echo $id ?>">
+                                                            <div class="button r" id="button-1">
+                                                                <input type="checkbox" onchange="doThis<?php echo $id ?>(this)" name="wegeb" id="wegep<?php echo $id ?>" value="<?php echo $value_visible ?>" class="checkbox" <?php echo $check_visible ?>  <?php echo $stt_form ?>/>
+                                                                <div class="knobs"></div>
+                                                                <div class="layer"></div>
+                                                            </div>
+                                                        </form>  
+                                                        <script>
+                                                            function doThis<?php echo $id ?>(checkbox){
+                                                            var wegeb   = document.getElementById("wegep<?php echo $id ?>").value
+                                                            var id      = document.getElementById("id<?php echo $id ?>").value
+                                                            var tbname  = document.getElementById("tbname<?php echo $id ?>").value
+                                                            var info = {wegeb: wegeb,id: id,tbname: tbname};
+                                                            $.ajax({
+                                                                type : "POST",
+                                                                url : "change_visible.php",
+                                                                data : info,
+                                                                success: function(){
+                                                                    document.getElementById("wegep<?php echo $id ?>").value = wegeb == 'Y'?'N':'Y'  
+                                                                },
+                                                                error: function(){
+                                                                     document.getElementById("wegep<?php echo $id ?>").value = wegeb
+                                                                }
+                                                                });
+                                                                return false;
+                                                            }   
+                                                        </script>   
+                                                    </td>
+                                                </tr>
+                                        <?php
+                                                $no++;
+                                            }
+                                        }
+                                        ?>            
+                                    </tbody>
+                                    </table>
+                                </div>
+                                <!-- /.card-body -->
+                            </div>
+                            <!-- /.card -->
+                        </div>
+                    </div>
+                    <!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
---
+++ administator/template/header.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <title>Katedral Administrator</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?php echo $base_assets ?>plugins/fontawesome-free/css/all.min.css">
+    <!-- daterange picker -->
+    <link rel="stylesheet" href="<?php echo $base_assets ?>plugins/datepicker/css/datepicker.css">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="<?php echo $base_assets ?>plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="<?php echo $base_assets ?>plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="<?php echo $base_assets ?>plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+    <!-- Toastr -->
+    <link rel="stylesheet" href="<?php echo $base_assets ?>plugins/toastr/toastr.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?php echo $base_assets ?>dist/css/adminlte.min.css">
+    <script type="text/javascript" src="<?php echo $base_assets ?>ckeditor/ckeditor.js"></script>
+    <script type="text/javascript" src="<?php echo $base_assets ?>plugins/jquery/jquery.min.js"></script>
+    <script type="text/javascript" src="<?php echo $base_assets ?>plugins/jquery-ui/jquery-ui.min.js"></script>
+    <link rel="stylesheet" href="<?php echo $base_assets ?>plugins/jquery-ui/jquery-ui.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.css" rel="stylesheet">  
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.js"></script>  
+    <link rel="stylesheet" href="<?php echo $base_assets ?>dist/css/dropzone-modif.css">
+    
+    <?php
+    if(isset($form))
+    {
+    ?>
+        <script type="text/javascript">
+
+        $(document).ready(function () {
+            // Handler for .ready() called.
+            $('html, body').animate({
+                scrollTop: $('#f<?php echo $form ?>').offset().top
+            }, 'slow');
+        });
+        </script>
+    <?php
+    }
+    ?>
+    <style>
+        
+        .toggle-button-cover {
+        display: table-cell;
+        position: relative;
+        width: 200px;
+        height: 140px;
+        box-sizing: border-box;
+        }
+
+        .button-cover {
+        height: 100px;
+        margin: 20px;
+        background-color: #fff;
+        box-shadow: 0 10px 20px -8px #c5d6d6;
+        border-radius: 4px;
+        }
+
+        .button-cover:before {
+        counter-increment: button-counter;
+        content: counter(button-counter);
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        color: #d7e3e3;
+        font-size: 12px;
+        line-height: 1;
+        padding: 5px;
+        }
+
+        .button-cover,
+        .knobs,
+        .layer {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        }
+
+        .button {
+        position: relative;
+        top: 50%;
+        width: 74px;
+        height: 36px;
+        margin: 0px auto 0 auto;
+        overflow: hidden;
+        }
+
+        .button.r,
+        .button.r .layer {
+        border-radius: 100px;
+        }
+
+        .button.b2 {
+        border-radius: 2px;
+        }
+
+        .checkbox {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        padding: 0;
+        margin: 0;
+        opacity: 0;
+        cursor: pointer;
+        z-index: 3;
+        }
+
+        .knobs {
+        z-index: 2;
+        }
+
+        .layer {
+        width: 100%;
+        background-color: #88A8D4;
+        transition: 0.3s ease all;
+        z-index: 1;
+        }
+
+        /* Button 1 */
+        #button-1 .knobs:before {
+        content: "ON";
+        position: absolute;
+        top: 3px;
+        left: 42px;
+        width: 30px;
+        height: 30px;
+        color: #fff;
+        font-size: 10px;
+        font-weight: bold;
+        text-align: center;
+        line-height: 1;
+        padding: 9px 4px;
+        background-color: #fff;
+        border-radius: 100%;
+        transition: 0.3s cubic-bezier(0.18, 0.89, 0.35, 1.15) all;
+        }
+
+        #button-1 .checkbox:checked + .knobs:before {
+        content: "OFF";
+        left: 4px;
+        background-color: #fff;
+        }
+
+        #button-1 .checkbox:checked ~ .layer {
+        background-color: #888888AD;
+        }
+
+        #button-1 .knobs,
+        #button-1 .knobs:before,
+        #button-1 .layer {
+        transition: 0.3s ease all;
+        }
+        .switch label input[type="checkbox"]:checked + .lever {
+            background-color: #DFDFDF;
+        }
+
+    </style>
+
+    <script type="text/javascript">
+      function checkAll(ele) {
+           var checkboxes = document.getElementsByTagName('input');
+           if (ele.checked) {
+               for (var i = 0; i < checkboxes.length; i++) {
+                   if (checkboxes[i].type == 'checkbox' ) {
+                       checkboxes[i].checked = true;
+                   }
+               }
+           } else {
+               for (var i = 0; i < checkboxes.length; i++) {
+                   if (checkboxes[i].type == 'checkbox') {
+                       checkboxes[i].checked = false;
+                   }
+               }
+           }
+       }
+    </script>
+</head>
+<div class="modal fade" id="notifmodal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body" style="text-align: center;vertical-align: middle;padding: 40px;">
+                <img src="" style="width: 70px;">
+                <br><br>
+                <h5>  </h5>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+
+<div class="modal fade" id="notifcancelmodal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body" style="text-align: center;vertical-align: middle;padding: 40px;">
+                <h3><b>Anda memiliki perubahan yang belum tersimpan</b></h3>
+                <br>
+                <p> Apabila anda meninggalkan halaman sekarang, perubahan anda akan hilang. Apakah anda ingin menyimpan sebelum meninggalkan halaman? </p>
+                <br>
+                <table width="100%">
+                    <tr>
+                        <td width="25%"> 
+                            <button id="btnmodalcancel" type="button" class="btn" style="background-color:#ffffff;color: #88A8D4;font-weight: bold;margin: 15px 0px;border-color: #88A8D4;">CANCEL</button>
+                        </td>
+                        <td width="75%" style="text-align:right"> 
+                            <button id="btnmodaldiscard" type="button" class="btn" style="background-color:#E90000;color: #ffffff;font-weight: bold;margin: 15px 0px;">DISCARD</button>
+                            &nbsp&nbsp&nbsp
+                            <button id="btnmodalsave" type="button" class="btn" style="background-color:#88A8D4;color: #ffffff;font-weight: bold;margin: 15px 0px;">SAVE</button>
+                        </td>
+                    </tr>
+                </table>        
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+
+<div class="modal fade" id="notifpublishmodal" style="pointer-events: none;">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body" style="text-align: center;vertical-align: middle;padding: 40px;">
+                <img src="assets/dist/img/icon_failed.png" style="width: 70px;">
+                <h3><b>Artikel tidak dapat di-publish, data belum lengkap</b></h3>
+                <br>
+                <p> Mohon melengkapi seluruh kolom dengan tanda <font color="red">*</font> sebelum mem-publish artikel. </p>
+                <button id="btnmodalclose" type="button" class="btn" style="background-color:#ffffff;color: #88A8D4;font-weight: bold;margin: 15px 0px;border-color: #88A8D4;">CLOSE</button>      
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+
+<?php
+    if(isset($form))
+    {
+    ?>
+        <script type="text/javascript">
+
+        $(document).ready(function () {
+            // Handler for .ready() called.
+            $('html, body').animate({
+                scrollTop: $('#f<?php echo $form ?>').offset().top
+            }, 'slow');
+        });
+        </script>
+    <?php
+    }
+    ?>
+<body class="hold-transition sidebar-mini">
